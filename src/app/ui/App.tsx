7c0f16af603de387ca6c369708a5fed3843c1b22
Richard Lopes <richard.lopes92@gmail.com>
2026-01-17 17:37:46 -0300

    Add empty pages for Home, Profile and Chat

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HomePage, ProfilePage, ChatPage } from '@/pages'
+
 function App() {
-  return <div></div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/chat/ui/ChatPage.tsx
@@ -0,0 +1,6 @@
+function ChatPage() {
+  return <div>Chat</div>
+}
+
+export { ChatPage }
+
